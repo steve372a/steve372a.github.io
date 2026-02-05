6d882de6e26152bc8cbe17d165e31352d629fdf0
huanbao3 <253563971@qq.com>
2026-02-05 18:32:04 +0800

    Site updated: 2026-02-05 18:32:03

--- pier/pier-2.0.0-beta1/share/other/copy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set "source=C:\Users\Steve372\Desktop\pier-latest\share\language\zh-CN\122"
-set "target=C:\Users\Steve372\Desktop\pier-latest\share\language\zh-CN\122\output\"
-
-if not exist "%target%" mkdir "%target%"
-
-for /r "%source%" %%f in (*.metadata) do (
-    copy "%%f" "%target%\%%~nxf" /y
-)
