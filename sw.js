ab78353650a3f3f0c6e33b989f7571de20938a1d
steve372a <steve372a@163.com>
2026-06-01 19:18:28 +0800

    Site updated: 2026-06-01 19:18:26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1779536831651";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1780312704197";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
