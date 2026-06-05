3ceb9e6e848311a69d96d9e33a374eec72aa0f7a
steve372a <steve372a@163.com>
2026-06-06 01:38:50 +0800

    Site updated: 2026-06-06 01:38:50

--- js/folder.js
+++
@@ -1,8 +0,0 @@
-(function (document) {
-    [].forEach.call(document.getElementsByClassName('fold'), function(panel) {
-        panel.getElementsByClassName('fold-title')[0].onclick = function() {
-            panel.classList.toggle("collapsed");
-            panel.classList.toggle("expanded");
-        }
-    });
-})(document);
